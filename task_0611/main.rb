33214068640d7a1814fcfaec970a72ec43a47443
konchanxxx <rexit.org@gmail.com>
2022-06-11 01:03:23 +0900

    fix output infomation

--- task_0611/main.rb
+++ task_0611/main.rb
@@ -1,12 +1,25 @@
 require "./team.rb"
 require "./player.rb"
 
-team = Team.new("セレッソ大阪", "J1", 1957, "ヨドコウ桜スタジアム")
-team.infomation
+class TeamSummary
+  attr_reader :team, :players
 
-[
-  Player.new("乾 貴士", "34歳", "MF", 8),
-  Player.new("清武 弘嗣", "32歳", "MF", 10)
-].each do |player|
-  player.infomation
+  def initialize(team, players)
+    @team = team
+    @players = players
+  end
+
+  def output
+    team.infomation
+    players.each(&:infomation)
+  end
 end
+
+summary = TeamSummary.new(
+  Team.new("セレッソ大阪", "J1", 1957, "ヨドコウ桜スタジアム"),
+  [
+    Player.new("乾 貴士", "34歳", "MF", 8),
+    Player.new("清武 弘嗣", "32歳", "MF", 10)
+  ]
+)
+summary.output
