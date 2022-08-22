130ab1961eb70138542a7dacebfa3fa73c4465a9
Aono05 <k.aono0522@gmail.com>
2022-08-22 20:16:41 +0900

    second

--- book5_6.rb
+++ book5_6.rb
@@ -13,11 +13,14 @@
 # buy_burger('cheese', drink: true, potato: true)
 
 #5.6.5 メソッド呼び出し時の{}省略
-def buy_burger(menu, options = {})
-  puts options
-end
+# def buy_burger(menu, options = {})
+#   puts options
+# end
 
-buy_burger('fish', {'drink'=>true, 'potato'=>false})
+# buy_burger('fish', {'drink'=>true, 'potato'=>false})
 
-buy_burger('fish', 'drink'=>true, 'potato'=>false)
+# buy_burger('fish', 'drink'=>true, 'potato'=>false)
 
+#5.6.7 ハッシュから配列へ、配列からハッシュへ
+currnencies = {japan: 'yen', us: 'dollar', india: 'rupee'}
+p currnencies.to_a
