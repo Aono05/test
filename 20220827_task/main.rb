6750b85dc5071a14fc5d634551aa0fbe62f3c232
Aono05 <k.aono0522@gmail.com>
2022-08-27 11:24:39 +0900

    fix input text

--- 20220827_task/main.rb
+++ 20220827_task/main.rb
@@ -2,13 +2,13 @@ require_relative "./bmi_presenter"
 require_relative "./bmi"
 require_relative "./user"
 
-puts "名前を入力してください"
+puts "名前を入力してください。"
 input_name = gets.chomp
 
-puts "身長を入力してください"
+puts "身長を入力してください。"
 input_height = gets.chomp.to_f
 
-puts "体重を入力してください"
+puts "体重を入力してください。"
 input_weight = gets.chomp.to_f
 
 user = User.new(input_name, input_height, input_weight)
