65117fc3b1027389635c71bf51d075cf1963234c
Aono05 <k.aono0522@gmail.com>
2022-08-27 11:00:10 +0900

    kg, m

--- task0827.rb
+++ task0827.rb
@@ -10,10 +10,10 @@ class Bmi
   end
 end
 
-puts "体重を入力してください" 
+puts "体重[kg]を入力してください" 
 user_input_weight = gets.chomp.to_f
 
-puts "身長を入力してください"
+puts "身長[m]を入力してください"
 user_input_height = gets.chomp.to_f
 
 puts "あなたのBMIは？"
