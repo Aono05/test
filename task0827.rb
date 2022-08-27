8884f67fc6d1151eb26f92f0387d40bcffc98652
Aono05 <k.aono0522@gmail.com>
2022-08-27 10:53:33 +0900

    Fix float

--- task0827.rb
+++ task0827.rb
@@ -11,10 +11,11 @@ class Bmi
 end
 
 puts "体重を入力してください" 
-user_input_weight = gets.chomp.to_i
+user_input_weight = gets.chomp.to_f
 
 puts "身長を入力してください"
-user_input_height = gets.chomp.to_i
+user_input_height = gets.chomp.to_f
 
+puts "あなたのBMIは？"
 test = Bmi.new(user_input_weight, user_input_height)
 puts test.calculation
